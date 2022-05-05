b1a01351478e5dd5be466bf49836708f7857ce24
Marzio95 <marziodellarocca14@gmail.com>
2022-05-05 17:58:59 +0200

    creo una migration in cui aggiungo la colonna data treno

--- app/Http/Controllers/TrainController.php
+++ app/Http/Controllers/TrainController.php
@@ -10,19 +10,19 @@ class TrainController extends Controller
     public function trainsList()
     {
 
-        $myTrain = new Train();
-        $myTrain->azienda = 'FrecciaArgento';
-        $myTrain->stazione_di_partenza = 'Roma';
-        $myTrain->stazione_di_arrivo = 'Bergamo';
-        $myTrain->orario_di_partenza = '11:00';
-        $myTrain->orario_di_arrivo = '16:00';
-        $myTrain->codice_treno = '456747862345';
-        $myTrain->numero_carrozze = '4';
-        $myTrain->in_orario = 'NO';
-        $myTrain->cancellato = 'SI';
-        $myTrain->prezzo_biglietto = '13.00';
+        // $myTrain = new Train();
+        // $myTrain->azienda = 'FrecciaArgento';
+        // $myTrain->stazione_di_partenza = 'Roma';
+        // $myTrain->stazione_di_arrivo = 'Bergamo';
+        // $myTrain->orario_di_partenza = '11:00';
+        // $myTrain->orario_di_arrivo = '16:00';
+        // $myTrain->codice_treno = '456747862345';
+        // $myTrain->numero_carrozze = '4';
+        // $myTrain->in_orario = 'NO';
+        // $myTrain->cancellato = 'SI';
+        // $myTrain->prezzo_biglietto = '13.00';
 
-        $myTrain->save();
+        // $myTrain->save();
 
 
         $myTrains = Train::all();
