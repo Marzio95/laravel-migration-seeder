eca42dfe4a08de2a6a7c7b398f33e1855debc5d6
Marzio95 <marziodellarocca14@gmail.com>
2022-05-05 17:32:48 +0200

    inserisco la funzione $myTrain->save() per creare row della tabella dal codice

--- app/Http/Controllers/TrainController.php
+++ app/Http/Controllers/TrainController.php
@@ -9,6 +9,22 @@ class TrainController extends Controller
 {
     public function trainsList()
     {
+
+        $myTrain = new Train();
+        $myTrain->azienda = 'Italo';
+        $myTrain->stazione_di_partenza = 'Roma';
+        $myTrain->stazione_di_arrivo = 'Firenze';
+        $myTrain->orario_di_partenza = '11:00';
+        $myTrain->orario_di_arrivo = '13:00';
+        $myTrain->codice_treno = '43583987';
+        $myTrain->numero_carrozze = '11';
+        $myTrain->in_orario = 'SI';
+        $myTrain->cancellato = 'NO';
+        $myTrain->prezzo_biglietto = '18.00';
+
+        $myTrain->save();
+
+
         $myTrains = Train::all();
         $data = [
             'myTrains' => $myTrains,
